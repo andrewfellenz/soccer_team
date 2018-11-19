a4b98a352074ea34c374d2e5803c72b39d18777f
Drew <andrewfellenz@gmail.com>
2018-11-18 22:37:31 -0500

    added comments and dunder

--- league_builder.py
+++ league_builder.py
@@ -1,3 +1,14 @@
+# Define a function to pull all the families into a dictionary of dictionaries
+# Define a function to divide the players into lists of experienced and inexperienced
+# Create the teams: Sharks, Dragons, and Raptors
+# Define a function that evenly distributes the players amongst the teams
+# Define a function that creates letters for each set of guardians and outputs it
+#### to a file named parentFirst_parentLast.txt
+# Define a function that creates a file of the team rosters, named teams.txt
+
+#### CODE MUST INCLUDE COMMENTS -- USE DOCSTRINGS AS WELL ####
+
+
 import csv
 
 
@@ -11,9 +22,15 @@ def get_families(family_list):
             families[count] = {}
             for key, value in row.items():
                 families[count][key] = value
-            count +=1
+            count += 1
         print(families)
         return families
 
 
-families = get_families('soccer_players.csv')
+def divide_by_skill(players):
+    for 
+
+if __name__ == "__main__":
+    families = get_families('soccer_players.csv')
+
+    divide_by_skill
